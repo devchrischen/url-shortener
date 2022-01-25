db45437361636f7523f8e12477a39aac18b33173
Chris Chen <chris.chen@myfunnow.com>
2022-01-26 00:28:44 +0800

    feat: add hash lib

---
+++ lib/hash/hash.go
@@ -0,0 +1,24 @@
+package hash
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const HASH_BASE = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func CreateSixDigitHash() string {
+	var hashes = make([]string, 6)
+	for i := 0; i < 6; i++ {
+		hashes[i] = chooseRandomDigit()
+	}
+	hashStr := strings.Join(hashes, "")
+	return hashStr
+}
+
+func chooseRandomDigit() string {
+	rand.Seed(time.Now().UnixNano())
+	randomDigit := string(HASH_BASE[rand.Intn(62)])
+	return randomDigit
+}
