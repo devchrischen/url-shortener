29b76807f5419908246887a21e4632ab74bef14b
Chris Chen <chris.chen@myfunnow.com>
2022-01-26 00:28:07 +0800

    feat: list handler implementation steps

--- api/url/create.go
+++ api/url/create.go
@@ -6,7 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Request struct {
+	OriginalUrl string `json:"original_url" form:"original_url"`
+}
+
 func CreateShortUrl(c *gin.Context) {
+	// validate request
+	var req Request
+	if err := c.ShouldBind(&req); err != nil {
+		panic(err)
+	}
+	// check duplicate => Input: url  Output: error
+
+	// produce unique hash => Input: nil  Output: hash string
+
+	// save original url and hash to database => Input: url, hash  Output: error
+
+	// return code, message, data(baseUrl + hash) as response
 
 	c.String(http.StatusOK, "short url created!")
 }
--- api/url/redirect.go
+++ api/url/redirect.go
@@ -7,5 +7,13 @@ import (
 )
 
 func Redirect(c *gin.Context) {
+	// validate request
+
+	// check param is valid hash
+
+	// query db to check if the data exist and not expired
+
+	// return code, message, data(redirect url) as response
+
 	c.String(http.StatusOK, "successfully redirect!")
 }
