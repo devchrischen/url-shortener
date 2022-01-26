884228150a7a44a5b68a5e9a16aa1a10353ef801
Chris Chen <chris.chen@myfunnow.com>
2022-01-26 18:44:00 +0800

    feat: add GetHashRecord func to url service

--- services/url/hash.go
+++ services/url/hash.go
@@ -5,3 +5,7 @@ import "github.com/devchrischen/url-shortener/entities/edb"
 func (s *Service) InsertHash(hash *edb.Hash) error {
 	return s.db.Create(hash).Error
 }
+
+func (s *Service) GetHashRecord(hash *edb.Hash, hashValue string) error {
+	return s.db.Where("value = ?", hashValue).Take(hash).Error
+}
