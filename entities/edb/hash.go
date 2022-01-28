9ae7da938159637fe789fde2ff55fa095aa0ade4
Chris Chen <chris.chen@myfunnow.com>
2022-01-28 14:57:11 +0800

    fix: fix Hash model struct tags

--- entities/edb/hash.go
+++ entities/edb/hash.go
@@ -4,7 +4,7 @@ import "time"
 
 type Hash struct {
 	ID        uint64    `gorm:"column:ID;primary_key"`
-	Value     string    `gorm:"column:value"`
+	Value     string    `gorm:"column:Value"`
 	CreatedAt time.Time `gorm:"column:CreatedAt"`
 	UpdatedAt time.Time `gorm:"column:UpdatedAt"`
 }
